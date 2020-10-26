14c81ee60432ee8e55e27b98aae4f63463a717cc
Steve Bargelt <steve@bargelt.com>
2020-10-26 05:03:59 -0700

    Added systemd support to relaylistener

--- Inferno.RelayListener/publish.sh
+++ Inferno.RelayListener/publish.sh
@@ -2,4 +2,8 @@ dotnet publish -r linux-arm
 scp pi@10.0.20.30:/home/pi/inferno/listener/appsettings.json ./temp.json
 scp -r ./bin/Debug/netcoreapp3.0/linux-arm/publish/* pi@10.0.20.30:~/inferno/listener
 scp ./temp.json pi@10.0.20.30:/home/pi/inferno/listener/appsettings.json
+#scp ./infernorelaylistener.service pi@10.0.20.30:/etc/systemd/system/infernorelaylistener.service
+# the above command fails... so copying to home...
+scp ./infernorelaylistener.service pi@10.0.20.30:/home/pi/infernorelaylistener.service
+ssh pi@10.0.20.30 sudo mv /home/pi/infernorelaylistener.service /etc/systemd/system/infernorelaylistener.service
 # rm temp.json
